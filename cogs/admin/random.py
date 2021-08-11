aaf906573f88c3616684d66094b60222893cde16
Rounak-Das-02 <62498036+Rounak-Das-02@users.noreply.github.com>
2021-08-11 11:52:33 +0530

    Minor Fixes Added more informative help command. Changed error embed colour to red.
    Signed-off-by: Rounak-Das-02 <62498036+Rounak-Das-02@users.noreply.github.com>

--- cogs/admin/random.py
+++ cogs/admin/random.py
@@ -9,7 +9,7 @@ class random(commands.Cog):
 		self.bot = bot
 
 
-	@commands.command(help = "Sends message anonymously   | sudo echo \"Your message\" ")
+	@commands.command(help = "Sends message anonymously  | sudo echo YourMessage ")
 	@has_permissions(administrator=True)
 	async def echo(self, ctx, *, message):
 		await ctx.message.delete()
@@ -18,17 +18,16 @@ class random(commands.Cog):
 
 	@echo.error
 	async def echo_error(self,ctx, error):
-		embed = discord.Embed(description="Hey! something went wrong",colour=discord.Colour.red())
+		embed = discord.Embed(description=f"○ No message entered.\n○ Try typing something after echo -> `sudo echo YourMessage`.\n○ Type `sudo help` to know more about each command.",colour=discord.Colour.red())
 		await ctx.channel.send(embed = embed)
 
-	@commands.command(pass_context = True ,help = "Purge messages   | sudo purge NumberOfMessagesToBeDeleted", aliases = ("clear", "cls"))
+	@commands.command(pass_context = True ,help = "Purge messages             | sudo purge AnInteger", aliases = ("clear", "cls"))
 	@has_permissions(administrator=True)
 	async def purge(self, ctx, limit: int):
 		await ctx.channel.purge(limit = limit+1)
-		
+
 	@purge.error
 	async def purge_error(self,ctx, error):
-		embed = discord.Embed(description="Hey! something went wrong",colour=discord.Colour.red())
 		count = 0
 		try:
 			id = int(ctx.message.reference.message_id)
@@ -42,14 +41,16 @@ class random(commands.Cog):
 						count+=1
 				await ctx.channel.purge(limit = count)
 			else:
+				embed = discord.Embed(description="Hey! something went wrong",colour=discord.Colour.red())
 				await ctx.channel.send(embed = embed)
 		except:
+			embed = discord.Embed(description=f"○ A parameter is missing.\n○ Try mentioning one integer -> `sudo purge Number`.\n○ Or else reply to a message and type -> `sudo purge` .\n○ Type `sudo help` to know more about each command.",colour=discord.Colour.red())
 			await ctx.channel.send(embed = embed)
 
 
 
 
-	@commands.command(pass_context = True ,help = "Purge messages of specific user   | sudo purge_user @mention NumberOfMessagesToBeDeleted", aliases = ("clear_user", "cls_user"))
+	@commands.command(pass_context = True ,help = "Purge messages of @user    | sudo purge_user @mention AnInteger", aliases = ("clear_user", "cls_user"))
 	@has_permissions(administrator=True)
 	async def purge_user(self, ctx, member:discord.Member, limit: int):
 		def is_member(m):
@@ -60,7 +61,7 @@ class random(commands.Cog):
 		
 	@purge_user.error
 	async def purge_user_error(self,ctx, error):
-		embed = discord.Embed(description="Hey! something went wrong",colour=discord.Colour.red())
+		embed = discord.Embed(description=f"○ Missing Parameter(s).\n○ Try mentioning user and provide an integer -> `sudo purge_user @user Number`.\n○ Type `sudo help` to know more  about each command.",colour=discord.Colour.red())
 		await ctx.channel.send(embed = embed)
 
 
