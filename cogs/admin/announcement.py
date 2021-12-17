b352935fecda8c6e4d8033bd38d39aee2fb08b5d
FloareDor <Ravicity999@gmail.com>
2021-12-17 20:02:29 +0530

    Fixed several bugs

--- cogs/admin/announcement.py
+++ cogs/admin/announcement.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions
 import json
-import uuid
+import shortuuid
 import datetime
 import asyncio
 import os
